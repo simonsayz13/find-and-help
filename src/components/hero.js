6519a0fa68aba5b350ebd9c18d59508a5f704163
Ross Middleton <5366498+R-Middleton@users.noreply.github.com>
2022-08-21 20:23:10 +0100

    removed todo list from hero

--- src/components/hero.js
+++ src/components/hero.js
@@ -2,21 +2,6 @@ import React from 'react';
 import OpportunityLg from './opportunityLg';
 
 const Hero = () => {
-  /**TODO:
-   * - [x] extract add opportunity button to component
-   * - style responsive version of Add opportunity button
-   * - use add VO button in hero and navbar
-   * - add background image to full size hero
-   * - full size hero in a box with image surround
-   * - add sliding filter sidebar component
-   * - responsive filter sidebar as folding mobile sidebar or full size
-   * - flesh out add opportunity modal
-   * - mock opportunities
-   * - add opportunity card to content
-   * - flesh out opportunity card
-   * - add responsive search bar
-   */
-
   return (
     <div className='hidden grid-in-hero md:hero '>
       <div className='hero-content flex-col'>
